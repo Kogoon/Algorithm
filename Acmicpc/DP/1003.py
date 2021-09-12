222903166a74ff449df0eb7f6e0b997a1f8a362f
Kogoon <gojs712@gmail.com>
2021-09-13 00:45:24 +0900

    2021.09.13 Dynamic Programming - Fibonacci

--- Acmicpc/DP/1003.py
+++ Acmicpc/DP/1003.py
@@ -1,16 +1,7 @@
 import sys 
 input = sys.stdin.readline
 
-def fibo(num):
-    if num == 0:
-        return 0
-    if num == 1:
-        return 1
-
-    return fibo(num-1) + fibo(num-2)
-
 T = int(input())   # Test case
-fibo_data = [0, 1]
 zero_data = [1, 0]
 one_data = [0, 1]
 
@@ -23,7 +14,6 @@ for _ in range(T): # 'T' times
         print('{} {}'.format(zero_data[num], one_data[num]))
     else:
         for i in range(2, num+1):
-            fibo_data.append(fibo_data[i-1]+fibo_data[i-2])
             zero_data.append(zero_data[i-1]+zero_data[i-2])
             one_data.append(one_data[i-1]+one_data[i-2])
-        print('{} {}'.format(zero_data[i], one_data[i]))
+        print('{} {}'.format(zero_data[num], one_data[num]))
